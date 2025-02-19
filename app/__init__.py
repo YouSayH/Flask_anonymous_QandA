f03f411ddeae02ddf60c9b48239a8df36ba95ca8
YouSayH <yusei.h24@kcr.ac.jp>
2025-02-19 21:45:36 +0900

    Update __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -1,10 +1,12 @@
 # app/__init__.py
+import os
 from flask import Flask
 from datetime import timedelta
+from werkzeug.middleware.proxy_fix import ProxyFix
 
 def create_app():
     app = Flask(__name__)
-    
+    app.wsgi_app = ProxyFix(app.wsgi_app, x_proto=1, x_host=1)
     # セッション情報の保護用の秘密鍵（本番環境では環境変数から読み込む等の対策を）
     app.secret_key = 'your_secret_key_here'
     
