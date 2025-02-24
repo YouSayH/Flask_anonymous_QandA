b8575585d34e572ad02025434ef8d64065403d13
YouSayH <yusei.h24@kcr.ac.jp>
2025-02-24 21:47:05 +0900

    Update run.py

--- run.py
+++ run.py
@@ -4,5 +4,5 @@ app = create_app()
 
 if __name__ == '__main__':
     # EC2上で動作させる場合、ホストは'0.0.0.0'に設定して外部からのアクセスを許可します。
-    # app.run(host='0.0.0.0', port=5000, debug=False)
-    app.run()
+    app.run(host='0.0.0.0', port=5000, debug=False)
+    # app.run()
